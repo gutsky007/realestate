b5d02773108b5b8ded219324e4c7ea2c7991ccc5
gutsky007 <nova27112000@gmail.com>
2024-10-09 22:41:59 +0700

    CRUD BlogPost

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ use App\Http\Controllers\Backend\PropertyTypeController;
 use App\Http\Controllers\Backend\PropertyController;
 use App\Http\Controllers\Backend\StateController;
 use App\Http\Controllers\Backend\TestimonialController;
+use App\Http\Controllers\Backend\BlogController;
 
 use App\Http\Controllers\Agent\AgentPropertyController;
 
@@ -183,6 +184,36 @@ Route::middleware(['auth','role:admin'])->group(function (){
 
     });
 
+ // Testimonials  All Route 
+Route::controller(TestimonialController::class)->group(function(){
+    Route::get('/all/testimonials', 'AllTestimonials')->name('all.testimonials'); 
+    Route::get('/add/testimonials', 'AddTestimonials')->name('add.testimonials');
+    Route::post('/store/testimonials', 'StoreTestimonials')->name('store.testimonials'); 
+    Route::get('/edit/testimonials/{id}', 'EditTestimonials')->name('edit.testimonials');
+    Route::post('/update/testimonials', 'UpdateTestimonials')->name('update.testimonials');
+    Route::get('/delete/testimonials/{id}', 'DeleteTestimonials')->name('delete.testimonials');  
+
+});    
+    // Blog Category All Route 
+Route::controller(BlogController::class)->group(function(){
+    Route::get('/all/blog/category', 'AllBlogCategory')->name('all.blog.category');  
+    Route::post('/store/blog/category', 'StoreBlogCategory')->name('store.blog.category'); 
+    Route::get('/blog/category/{id}', 'EditBlogCategory');
+    Route::post('/update/blog/category', 'UpdateBlogCategory')->name('update.blog.category');
+    Route::get('/delete/blog/category/{id}', 'DeleteBlogCategory')->name('delete.blog.category');  
+});
+
+
+    // BlogPost  All Route 
+Route::controller(BlogController::class)->group(function(){
+    Route::get('/all/post', 'AllPost')->name('all.post'); 
+    Route::get('/add/post', 'AddPost')->name('add.post');
+    Route::post('/store/post', 'StorePost')->name('store.post');
+    Route::get('/edit/post/{id}', 'EditPost')->name('edit.post');
+    Route::post('/update/post', 'UpdatePost')->name('update.post');
+    Route::get('/delete/post/{id}', 'DeletePost')->name('delete.post');  
+});
+
 });//END GROUP ADMIN MIDDLEWARE
 
 // Frontend Property Details All Route 
@@ -225,13 +256,4 @@ Route::post('/rent/property/search', [IndexController::class, 'RentPropertySearc
 Route::post('/all/property/search', [IndexController::class, 'AllPropertySearch'])->name('all.property.search');
 
 
- // Testimonials  All Route 
-Route::controller(TestimonialController::class)->group(function(){
-    Route::get('/all/testimonials', 'AllTestimonials')->name('all.testimonials'); 
-    Route::get('/add/testimonials', 'AddTestimonials')->name('add.testimonials');
-    Route::post('/store/testimonials', 'StoreTestimonials')->name('store.testimonials'); 
-    Route::get('/edit/testimonials/{id}', 'EditTestimonials')->name('edit.testimonials');
-    Route::post('/update/testimonials', 'UpdateTestimonials')->name('update.testimonials');
-    Route::get('/delete/testimonials/{id}', 'DeleteTestimonials')->name('delete.testimonials');  
- 
-});
+
